91b373964bf036179b7c142571912a643f9f4e07
Mujahid Hussain Awan <awan.nedian@gmail.com>
2023-04-19 14:37:01 +0400

    update

---
+++ Part_1/routes/routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getBlog,
+  addBlog,
+  getBlogId,
+  deleteBlog,
+  updateBlog,
+  login
+} = require("../controllers/blogs");
+
+router.get("/", getBlog);
+
+router.post("/", addBlog);
+
+router.get("/:id", getBlogId);
+
+router.delete("/:id", deleteBlog);
+
+router.put("/:id", updateBlog);
+
+router.post("/login", login);
+
+
+module.exports = router;
